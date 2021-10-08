74af416696ac9f55f1f89648972fe9c517696d28
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2021-10-08 17:23:36 +0200

    Added Cinemachine, worked on camera scene and folder organization

--- Assets/Prototype Assets/PlaceHolderMOVEMENT.cs
+++ Assets/Prototype Assets/PlaceHolderMOVEMENT.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlaceHolderMOVEMENT : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
+    public CharacterController controller;
+    public float speed = 6f;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+
+        //GET AXIS MOVEMENT
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertiacal = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertiacal).normalized;
+
+        //GET AXIS MOUSE MOVEMENT 2AXIS BASED 
+        float MousedeltaX = Input.GetAxis("Mouse X");
+        float MousedeltaY = Input.GetAxis("Mouse Y");
+
+
+       // CHARACTER Y ROTATION TO MOUSE
+        if (MousedeltaX>0.1f || MousedeltaY>0.1f)
+        {
+
+            Vector3 MousePositionWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 DirectionToMouse = new Vector3(MousePositionWorld.x-transform.position.x,MousePositionWorld.y-transform.position.y,MousePositionWorld.z-transform.position.z).normalized;
+
+            float targetangle = Mathf.Atan2(DirectionToMouse.x, DirectionToMouse.z) *Mathf.Rad2Deg;
+
+
+            transform.rotation = Quaternion.Euler(0f,targetangle,0f);
+        }
+
+
+        if (direction.magnitude>=0.1f)
+        {
+
+           controller.Move(direction * speed * Time.deltaTime);
+
+        }
+
+
+
+
+        
+       
+        
         
     }
 }
